6253625a73c727174126794f8e1c0d4d94d8c08b
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-15 20:49:40 +0600

    application count added

--- index.js
+++ index.js
@@ -81,6 +81,29 @@ async function run() {
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application);
+
+      // not the best way (use aggregate)
+      const id = application.job_id;
+      const query = {_id: new ObjectId(id)}
+      const job = await jobsCollection.findOne(query);
+      let newCount = 0;
+      if(job.applicationCount){
+        newCount = job.applicationCount + 1;
+      }
+      else{
+        newCount = 1;
+      }
+
+      console.log(job);
+
+      // now update the job info
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount
+        }
+      }
+      const updateResult = await jobsCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
